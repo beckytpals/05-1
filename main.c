1f8a9010e624fd193631dd762069ad3be562c3f8
beckytpals <dlk1020@sookmyung.ac.kr>
2025-10-02 16:25:58 +0900

    05-5

--- main.c
+++ main.c
@@ -4,18 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {	
-	int x;
-	int i;
-	int sum = 0;
+	int a,b;
+	char c;
 	
-	printf("Input an integer : ");
-	scanf("%d", &x);
+	printf("enter the calculation : ");
+	scanf("%i %c %i", &a, &c, &b);
 	
-	for(i=1; i <= x; i++)
-			sum = sum + i; 
-	
-	printf("sum is %i\n", sum);
-
+	if(c == '+')
+	printf(" = %i\n", a+b);
+	else if (c == '-')
+	printf(" = %i\n", a-b);
+	else if (c == '*')
+	printf(" = %i\n", a*b);
+	else if (c == '/');
+	printf(" = %i\n", a/b);
 
 	return 0;
 }
